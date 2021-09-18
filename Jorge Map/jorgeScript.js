296ca7e3f5ff79689829fa01f3fa113468fe996a
Scoutbirds <70183932+Scoutbirds@users.noreply.github.com>
2021-09-17 22:46:11 -0400

    Implmented the remaining data and made a new UI

--- Jorge Map/jorgeScript.js
+++ Jorge Map/jorgeScript.js
@@ -4,13 +4,13 @@ var map = new mapboxgl.Map({
 container: "map",
 style: "mapbox://styles/scoutbirds/cktmgxnmt2tjy17o3tovbe9k0",
 center: [-80.15, 25.85], // starting position [lng, lat]
-zoom: 10.5, // starting zoom
+zoom: 12, // starting zoom
 });
 map.addControl(new mapboxgl.NavigationControl());
 
 
 
-var data = ["Temp", "Sal_Data", "ODO","Chlor_Data","Turb_Data", "pH"];
+var data = ["Temp", "Salinity", "ODO","Chlorophyll","Turbidity", "pH"];
 var Bdata = ["Buoy_Temp", "Buoy_Sal", "Buoy_ODO", "Buoy_Chlor", "Buoy_Turb", "Buoy_PH"];
 const layer = document.getElementById("layer");
 
@@ -24,132 +24,70 @@ map.setPaintProperty("Buoy_ODO", "circle-opacity", 0.5);
 map.setPaintProperty("biscayne-bay-data", "circle-opacity", 0.1);
 });
 
+const gradient = document.getElementById("legend")
 
-viewButton.addEventListener("click", function () {
-for (var i = 0; i < data.length; i++) {     
 
-    if(document.getElementById("Fish").checked)
-    {
+TempButton.addEventListener("click", function()
+{
+    map.setPaintProperty("Temp", "circle-opacity", 1);
+    map.setPaintProperty("Salinity", "circle-opacity", 0);
+    map.setPaintProperty("ODO", "circle-opacity", 0);
+    map.setPaintProperty("Chlorophyll", "circle-opacity", 0);
+    map.setPaintProperty("Turbidity", "circle-opacity", 0);
+    map.setPaintProperty("pH", "circle-opacity", 0);
 
-        if (layer.value === "Temp") {
-        map.setPaintProperty("Temp", "circle-opacity", 0.5);
-        map.setPaintProperty("Sal_Data", "circle-opacity", 0);
-        map.setPaintProperty("ODO", "circle-opacity", 0);
-        map.setPaintProperty("Chlor_Data", "circle-opacity", 0);
-        map.setPaintProperty("Turb_Data", "circle-opacity", 0);
-        map.setPaintProperty("PH_Data", "circle-opacity", 0);
-        } 
-        if (layer.value === "Sal") {
-        map.setPaintProperty("Temp_Data", "circle-opacity", 0);
-        map.setPaintProperty("Sal_Data", "circle-opacity", 0.5);
-        map.setPaintProperty("ODO_Data", "circle-opacity", 0);
-        map.setPaintProperty("Chlor_Data", "circle-opacity", 0);
-        map.setPaintProperty("Turb_Data", "circle-opacity", 0);
-        map.setPaintProperty("pH", "circle-opacity", 1);
-        } 
-        if (layer.value === "ODO") {
-        map.setPaintProperty("Temp_Data", "circle-opacity", 0);
-        map.setPaintProperty("Sal_Data", "circle-opacity", 0);
-        map.setPaintProperty("ODO", "circle-opacity", 0.5);
-        map.setPaintProperty("Chlor_Data", "circle-opacity", 0);
-        map.setPaintProperty("Turb_Data", "circle-opacity", 0);
-        map.setPaintProperty("PH_Data", "circle-opacity", 0);
-        }
-        if (layer.value === "Chlor") 
-        {
-        map.setPaintProperty("Temp_Data", "circle-opacity", 0);
-        map.setPaintProperty("Sal_Data", "circle-opacity", 0);
-        map.setPaintProperty("ODO_Data", "circle-opacity", 0);
-        map.setPaintProperty("Chlor_Data", "circle-opacity", 0.5);
-        map.setPaintProperty("Turb_Data", "circle-opacity", 0);
-        map.setPaintProperty("PH_Data", "circle-opacity", 0);
-        }
-        if (layer.value === "Turb") 
-        {
-        map.setPaintProperty("Temp_Data", "circle-opacity", 0);
-        map.setPaintProperty("Sal_Data", "circle-opacity", 0);
-        map.setPaintProperty("ODO_Data", "circle-opacity", 0);
-        map.setPaintProperty("Chlor_Data", "circle-opacity", 0);
-        map.setPaintProperty("Turb_Data", "circle-opacity", 0.5);
-        map.setPaintProperty("PH_Data", "circle-opacity", 0);
-        }
-        if (layer.value === "PH") 
-        {
-        map.setPaintProperty("Temp", "circle-opacity", 0);
-        map.setPaintProperty("Sal_Data", "circle-opacity", 0);
-        map.setPaintProperty("ODO", "circle-opacity", 0);
-        map.setPaintProperty("Chlor_Data", "circle-opacity", 0);
-        map.setPaintProperty("Turb_Data", "circle-opacity", 0);
-        map.setPaintProperty("pH", "circle-opacity", 0.5);
-        }
-        else {
-            map.setPaintProperty(data[i], "circle-opacity", 0);
-        }
 
-    }else{
-    map.setPaintProperty(data[i], "circle-opacity", 0);
-    }
-}
+    document.querySelector('#gradient').classList.add('hello');
+} )
 
-for (var i = 0; i < Bdata.length; i++) {     
+pHButton.addEventListener("click", function()
+{
+    map.setPaintProperty("Temp", "circle-opacity", 0);
+    map.setPaintProperty("Salinity", "circle-opacity", 0);
+    map.setPaintProperty("ODO", "circle-opacity", 0);
+    map.setPaintProperty("Chlorophyll", "circle-opacity", 0);
+    map.setPaintProperty("Turbidity", "circle-opacity", 0);
+    map.setPaintProperty("pH", "circle-opacity", 1);
+} )
 
-if(document.getElementById("Buoys").checked){
 
-    if (layer.value === "Temp") {
-        map.setPaintProperty("Buoy_Temp", "circle-opacity", 0.5);
-        map.setPaintProperty("Buoy_Sal", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_ODO", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_Chlor", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_Turb", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_PH", "circle-opacity", 0);
-    } 
-    if (layer.value === "Sal") {
-        map.setPaintProperty("Buoy_Temp", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_Sal", "circle-opacity", 0.5);
-        map.setPaintProperty("Buoy_ODO", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_Chlor", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_Turb", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_PH", "circle-opacity", 0);
+ODOButton.addEventListener("click", function()
+{
+    map.setPaintProperty("Temp", "circle-opacity", 0);
+    map.setPaintProperty("Salinity", "circle-opacity", 0);
+    map.setPaintProperty("ODO", "circle-opacity", 1);
+    map.setPaintProperty("Chlorophyll", "circle-opacity", 0);
+    map.setPaintProperty("Turbidity", "circle-opacity", 0);
+    map.setPaintProperty("pH", "circle-opacity", 0);
+} )
 
-    } 
-    if (layer.value === "ODO") {
-        map.setPaintProperty("Buoy_Temp", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_Sal", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_ODO", "circle-opacity", 0.5);
-        map.setPaintProperty("Buoy_Chlor", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_Turb", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_PH", "circle-opacity", 0);
-    }
-    if (layer.value === "Chlor") {
-        map.setPaintProperty("Buoy_Temp", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_Sal", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_ODO", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_Chlor", "circle-opacity", 0.5);
-        map.setPaintProperty("Buoy_Turb", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_PH", "circle-opacity", 0);
-    }
-    if (layer.value === "Turb") {
-        map.setPaintProperty("Buoy_Temp", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_Sal", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_ODO", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_Chlor", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_Turb", "circle-opacity", 0.5);
-        map.setPaintProperty("Buoy_PH", "circle-opacity", 0);
-    }
-    if (layer.value === "PH") {
-        map.setPaintProperty("Buoy_Temp", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_Sal", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_ODO", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_Chlor", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_Turb", "circle-opacity", 0);
-        map.setPaintProperty("Buoy_PH", "circle-opacity", 0.5);
-    }
-        else {
-        map.setPaintProperty(Bdata[i], "circle-opacity", 0);
-    }
 
-}else{
-    map.setPaintProperty(Bdata[i], "circle-opacity", 0);
-}
-}
-});
+SalButton.addEventListener("click", function()
+{
+    map.setPaintProperty("Temp", "circle-opacity", 0);
+    map.setPaintProperty("Salinity", "circle-opacity", 1);
+    map.setPaintProperty("ODO", "circle-opacity", 0);
+    map.setPaintProperty("Chlorophyll", "circle-opacity", 0);
+    map.setPaintProperty("Turbidity", "circle-opacity", 0);
+    map.setPaintProperty("pH", "circle-opacity", 0);
+} )
+
+ChButton.addEventListener("click", function()
+{
+    map.setPaintProperty("Temp", "circle-opacity", 0);
+    map.setPaintProperty("Salinity", "circle-opacity", 0);
+    map.setPaintProperty("ODO", "circle-opacity", 0);
+    map.setPaintProperty("Chlorophyll", "circle-opacity", 1);
+    map.setPaintProperty("Turbidity", "circle-opacity", 0);
+    map.setPaintProperty("pH", "circle-opacity", 0);
+} )
+
+TurButton.addEventListener("click", function()
+{
+    map.setPaintProperty("Temp", "circle-opacity", 0);
+    map.setPaintProperty("Salinity", "circle-opacity", 0);
+    map.setPaintProperty("ODO", "circle-opacity", 0);
+    map.setPaintProperty("Chlorphyll", "circle-opacity", 0);
+    map.setPaintProperty("Turbidity", "circle-opacity", 1);
+    map.setPaintProperty("pH", "circle-opacity", 0);
+} )
