b2a303d10427658e7266293f6b5da74efd17badb
Scoutbirds <70183932+Scoutbirds@users.noreply.github.com>
2021-09-27 18:30:55 -0400

    More Styles

--- Jorge Map/jorgeScript.js
+++ Jorge Map/jorgeScript.js
@@ -7,26 +7,49 @@ center: [-80.15, 25.85], // starting position [lng, lat]
 zoom: 12, // starting zoom
 });
 map.addControl(new mapboxgl.NavigationControl());
-map.scrollZoom.disable();
+//map.scrollZoom.disable(); //disables the scroll zoom function
+
+
+
+
+const coralGables = new mapboxgl.Marker()
+    .setLngLat([-80.26966, 25.72695])
+    .addTo(map);
+
+// Buoy 2
+const northBiscayneBay = new mapboxgl.Marker()
+    .setLngLat([-80.16712, 25.86171])
+    .addTo(map);
+
+// Buoy 3
+const littleRiver = new mapboxgl.Marker()
+    .setLngLat([-80.176921, 25.846346])
+    .addTo(map);
+
+
 
 
 
 var data = ["Temp", "Salinity", "ODO","Chlorophyll","Turbidity", "pH"];
-var Bdata = ["Buoy_Temp", "Buoy_Sal", "Buoy_ODO", "Buoy_Chlor", "Buoy_Turb", "Buoy_PH"];
-const layer = document.getElementById("layer");
 
+/*For loop that turns all the data's opacity to zero*/
 map.on('load', function () {
 for(var i = 0; i < data.length; i++){
     map.setPaintProperty(data[i], "circle-opacity", 0);
-    map.setPaintProperty(Bdata[i], "circle-opacity", 0);
 }
-map.setPaintProperty("ODO", "circle-opacity", 1);
-map.setPaintProperty("Buoy_ODO", "circle-opacity", 0.5);
-map.setPaintProperty("biscayne-bay-data", "circle-opacity", 0.1);
+map.setPaintProperty("ODO", "circle-opacity", 1); //turns Oxygen dissolved data opacity to 1
 });
 
 const gradient = document.getElementById("legend")
 
+mapbutton.addEventListener("click", function(){
+window.scrollTo(
+    {
+        top: 1800,
+        left:0,
+        behavior:"smooth",
+    });
+})
 
 TempButton.addEventListener("click", function()
 {
