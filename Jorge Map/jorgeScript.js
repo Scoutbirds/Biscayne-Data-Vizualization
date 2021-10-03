0413e9ae5a993e45106bdb066207faca49444b73
dchev001 <dchev001@gmail.com>
2021-10-02 22:29:09 -0400

    map key legend (main)
    Used Julians map as reference for placement of the map key legend. The legend displays dynamically the data ranges.

--- Jorge Map/jorgeScript.js
+++ Jorge Map/jorgeScript.js
@@ -7,26 +7,49 @@ center: [-80.15, 25.85], // starting position [lng, lat]
 zoom: 12, // starting zoom
 });
 map.addControl(new mapboxgl.NavigationControl());
-map.scrollZoom.disable();
+//map.scrollZoom.disable(); //disables the scroll zoom function
+
+
+
+
+const coralGables = new mapboxgl.Marker()
+    .setLngLat([-80.26966, 25.72695])
+    .addTo(map);
+
+// Buoy 2
+const northBiscayneBay = new mapboxgl.Marker()
+    .setLngLat([-80.16712, 25.86171])
+    .addTo(map);
+
+// Buoy 3
+const littleRiver = new mapboxgl.Marker()
+    .setLngLat([-80.176921, 25.846346])
+    .addTo(map);
+
+
 
 
 
 var data = ["Temp", "Salinity", "ODO","Chlorophyll","Turbidity", "pH"];
-var Bdata = ["Buoy_Temp", "Buoy_Sal", "Buoy_ODO", "Buoy_Chlor", "Buoy_Turb", "Buoy_PH"];
-const layer = document.getElementById("layer");
 
+/*For loop that turns all the data's opacity to zero*/
 map.on('load', function () {
 for(var i = 0; i < data.length; i++){
     map.setPaintProperty(data[i], "circle-opacity", 0);
-    map.setPaintProperty(Bdata[i], "circle-opacity", 0);
 }
-map.setPaintProperty("ODO", "circle-opacity", 1);
-map.setPaintProperty("Buoy_ODO", "circle-opacity", 0.5);
-map.setPaintProperty("biscayne-bay-data", "circle-opacity", 0.1);
+map.setPaintProperty("ODO", "circle-opacity", 1); //turns Oxygen dissolved data opacity to 1
 });
 
 const gradient = document.getElementById("legend")
 
+mapbutton.addEventListener("click", function(){
+window.scrollTo(
+    {
+        top: 1800,
+        left:0,
+        behavior:"smooth",
+    });
+})
 
 TempButton.addEventListener("click", function()
 {
@@ -92,3 +115,96 @@ TurButton.addEventListener("click", function()
     map.setPaintProperty("Turbidity", "circle-opacity", 1);
     map.setPaintProperty("pH", "circle-opacity", 0);
 } )
+
+
+///=========================================================================
+// to change the map legend key
+function changeLegend(val)
+{
+  document.getElementById("map-overlay-legend").style.display = "block";
+
+  z = document.getElementById("gradNum");
+  y = document.getElementsByClassName("gradBox");
+  
+
+  var arr = [];
+  var txt = '';  
+
+  if (val == 'temp_level')
+  { 
+    arr = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+    col = ['blue', 'red', 'orange', 'pink', 'yellow', 'black', 'purple', 'navy', 'red', 'blue'];
+
+    for (x in arr, col)
+    { 
+      z.innerHTML = txt + '<div class=\"row\"><div class=\"gradBox\"></div><div class=\"label\"><p>' + arr[x] + '</p></div></div>';
+      y[x].style.background = col[x];
+      txt = z.innerHTML;
+    }
+  }
+
+  if (val == 'pH_level')
+  { 
+    arr = ['0-1', '1-2', '2-3', '3-4', '4-5'];
+    col = ['red', 'orange', 'orange', 'orange', 'yellow'];
+
+    for (x in arr, col)
+    {
+      z.innerHTML = txt + '<div class=\"row\"><div class=\"gradBox\"></div><div class=\"label\"><p>' + arr[x] + '</p></div></div>';
+      y[x].style.background = col[x];
+      txt = z.innerHTML;
+    }
+  }
+  
+  if (val == 'odo_level')
+  { 
+    arr = ['<1', '1-2', '2-3', '3-4', '4-5', '5-6', '6-7', '7-8', '8-9', '9-10', '10-11', '11-12', '12>'];
+    col = ['purple', 'red', 'orange', 'pink', 'yellow', 'black', 'blue', 'violet', 'green', 'red', 'blue', 'orange', 'green'];
+
+    for (x in arr, col)
+    {
+      z.innerHTML = txt + '<div class=\"row\"><div class=\"gradBox\"></div><div class=\"label\"><p>' + arr[x] + '</p></div></div>';
+      y[x].style.background = col[x];
+      txt = z.innerHTML;
+    }
+  }
+  
+  if (val == 'sal_level')
+  { 
+    arr = [1, 2, 3, 4];
+    col = ['red', 'blue', 'pink', 'yellow'];
+
+    for (x in arr, col)
+    {
+      z.innerHTML = txt + '<div class=\"row\"><div class=\"gradBox\"></div><div class=\"label\"><p>' + arr[x] + '</p></div></div>';
+      y[x].style.background = col[x];
+      txt = z.innerHTML;
+    }
+  }
+
+  if (val == 'ch_level')
+  { 
+    arr = [ 1, 2, 3, 4, 5];
+    col = ['red', 'white', 'blue', 'green', 'black'];
+
+    for (x in arr, col)
+    {
+      z.innerHTML = txt + '<div class=\"row\"><div class=\"gradBox\"></div><div class=\"label\"><p>' + arr[x] + '</p></div></div>';
+      y[x].style.background = col[x];
+      txt = z.innerHTML;
+    }
+  }
+  
+  if (val == 'tur_level')
+  { 
+    arr = [1, 2, 3];
+    col = ['red', 'white', 'blue'];
+
+    for (x in arr, col)
+    {
+      z.innerHTML = txt + '<div class=\"row\"><div class=\"gradBox\"></div><div class=\"label\"><p>' + arr[x] + '</p></div></div>';
+      y[x].style.background = col[x];
+      txt = z.innerHTML;
+    }
+  }  
+}
