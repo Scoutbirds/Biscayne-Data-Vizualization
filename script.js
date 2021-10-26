4056a349665b1a23e17f754fc69a66e8cf41db8e
J F <jfern075@fiu.edu>
2021-10-26 14:49:32 -0400

    Multiple legends done

--- script.js
+++ script.js
@@ -124,26 +124,28 @@ function toggleOff(idx) {
 function changeLegend(val) {
 
     var vals = ["temp_level", "odo_level", "pH_level", "sal_level", "ch_level", "tur_level"];
-    var val_includes = ['\"tempbar\"','\"ODObar\"','\"pHbar\"','\"salbar\"', '\"chlbar\"','\"turbar\"'];
+    var bars = ["tempbar", "ODObar", "pHbar", "salbar", "chlbar", "turbar"];
     var rows = ["tempRow", "ODORow", "pHRow", "salRow", "chlRow", "turRow"];
     var row_labels = ["Temperature", "Dissolved Oxygen", "pH", "Salinity", "Chlorophyll", "Turbidity"];
-    var bars = ["tempbar", "ODObar", "pHbar", "salbar", "chlbar", "turbar"];
-    var left_legend = ["Low", "High", "Acidic", "Low", "Low", "Low"];
-    var right_legend = ["High", "Low", "Alkaline", "High", "High", "High"];
+    var left_legend = ["Low", "High", "Alkaline", "Low", "Low", "Low"];
+    var right_legend = ["High", "Low", "Acidic", "High", "High", "High"];
     var row_gradients = ["linear-gradient(to left, #FA0000, #78F7F7)", "linear-gradient(to left, #F7EE69, #65D350)", "linear-gradient(to left, #FF910A, #1C1D4F)", "linear-gradient(to left, #F4156B, #9DA4D8)", "linear-gradient(to left, #4D16DA, #42D01B)", "linear-gradient(to left, #9BF40B, #5AEFF6)"];
+    
+    // Grab map-overlay-legend div, set display to block 
     var legendLabel = document.getElementById("map-overlay-legend");
     legendLabel.style.display = "block";
     
+    // Fetch index with input val
     var idx;
-    for(var i = 0; i < vals.length; i++){
+    for(var i = 0; i < vals.length; i++) {
         if (val === vals[i])
             idx = i;
     }
 
-    if (!(legendLabel.innerHTML.includes(val_includes[idx]))) {
+    // Use index value and arrays to populate legends
+    if (!(legendLabel.innerHTML.includes(bars[idx]))) {
         legendLabel.innerHTML += '<div class="row" id=\"' + rows[idx] + '\"><div class="label"><p class="label">' + row_labels[idx] + '</p></div><div id="legend" class="legend"><div class="bar" id=\"' + bars[idx] + '\"></div><div class="gradContainer" id="gradNum"></div><div class="legendtext"><p class="leftlegendtext">' + left_legend[idx] + '</p><p class="rightlegendtext">' + right_legend[idx] + '</p></div></div></div>';
-        var t = document.getElementById(bars[idx]);
-        t.style.background = row_gradients[idx];
+        document.getElementById(bars[idx]).style.background = row_gradients[idx];
     } else {
         document.getElementById(rows[idx]).remove();
         if (!(legendLabel.innerHTML.includes('\"row\"'))) {
